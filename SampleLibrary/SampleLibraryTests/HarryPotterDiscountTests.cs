ff1ca572e194650a25a4f84b00814adeb7e6a2d2
johnsonliao <johnson.liao@thinkpower.com.tw>
2016-01-17 15:01:58 +0800

    buy first book

---
+++ SampleLibrary/SampleLibraryTests/HarryPotterDiscountTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleLibrary.Tests
+{
+    [TestClass()]
+    public class HarryPotterDiscountTests
+    {
+        [TestMethod()]
+        public void GetBuyFirstBookTest()
+        {
+            var target = new HarryPotterDiscount();
+
+            var expected = 100;
+
+            var actual = target.BuyFirstBook();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
